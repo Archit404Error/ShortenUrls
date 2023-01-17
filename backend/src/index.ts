ee71df0f8c854621e60419ca533b8aa1680be997
Archit404Error <4architmehta@gmail.com>
2023-01-17 12:07:02 -0500

    Add MVC to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
+import userRouter from "./users/views";
 
 const app = express();
+app.use(bodyParser.json());
+app.use("/users", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(process.env.PORT || 3000, () => {
