d67f725354bb074da01d5c048ba135fea3eec429
Archit404Error <4architmehta@gmail.com>
2023-01-17 11:52:10 -0500

    Add TS backend

---
+++ backend/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("✅ Server is up and running");
+});
