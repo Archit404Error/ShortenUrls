6dd9541d203b4c9ab9e38332574478c239ca7e6a
Archit404Error <4architmehta@gmail.com>
2023-01-18 21:47:16 -0500

    Move to swagger autogen and ui

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import userRouter from "./users/views";
 import customerRouter from "./customers/views";
+import swaggerUI from "swagger-ui-express";
+import spec from "../api-spec.json";
 import { dbConnect } from "./database";
 
 const app = express();
 
 // Middleware to parse json request bodies
 app.use(bodyParser.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(spec));
 
 /**
  * Sub-routers for our main router, we should have one sub-router per "entity" in the application
