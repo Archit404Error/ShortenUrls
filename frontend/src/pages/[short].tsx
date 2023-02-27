b9f0935312db502950ad515820fbdfaaa188053e
Archit404Error <4architmehta@gmail.com>
2023-02-27 15:02:07 -0500

    Add redirect error

--- frontend/src/pages/[short].tsx
+++ frontend/src/pages/[short].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const fetchOrigUrl = async (shortUrl: string) => {
     const res = await fetch(`${process.env.SERVER}/links/${shortUrl}`);
@@ -9,6 +9,7 @@ const fetchOrigUrl = async (shortUrl: string) => {
 const Short = () => {
     const router = useRouter();
     const { short } = router.query;
+    const [message, setMessage] = useState(`Redirecting...`)
 
     useEffect(() => {
         if (!short) return;
@@ -16,10 +17,12 @@ const Short = () => {
             const orig = await fetchOrigUrl(short as string);
             if (orig.success)
                 window.location.assign(orig.data);
+            else
+                setMessage("Err: Invalid link")
         })()
     }, [short]);
 
-    return <h1>Redirecting to {short}</h1>;
+    return <h1>{message}</h1>;
 }
 
 export default Short;
